8fa9f8397f31041e5ec029eae0c18f81a14e8085
Cédric Néhémie <cartel.com@free.fr>
2011-02-01 14:43:52 +0100

    - Committed the omitted settings for the posts app

---
+++ posts/settings.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from django.conf import settings
+from django.utils.translation import ugettext as _
+from django.db.models import Q
+
+from abe.posts.models import *
+from abe.utils import *
+
+def default_post_page_meta_description_processor ( results ):
+	p = Post.objects.get( Q(published_date__year=results.group('year')) ,
+									  Q(published_date__month=results.group('month')),
+									  Q(published_date__day=results.group('day')),
+									  Q(slug=results.group('slug')) )
+	if p : 
+		return remove_html_tags(p.content_or_excerpt())[:50]
+	else:
+		return DEFAULT_META_DESCRIPTION
+
+NUM_POST_PER_PAGE =  getattr( settings, "NUM_POST_PER_PAGE","4")
+START_PAGE =  getattr( settings, "START_PAGE","1")
+
+DEFAULT_META_DESCRIPTION =  getattr( settings, "DEFAULT_META_DESCRIPTION",_(u"This is a default description for this page.") )
+URLS_META_DESCRIPTION =  getattr( settings, 
+														 "URLS_META_DESCRIPTION",
+														 (
+															("^/site/$", _(u"Welcome on the homepage of this site." ) ), 
+															("post/(?P<year>\d{4})/(?P<month>\d{1,2})/(?P<day>\d{1,2})/(?P<slug>[\w_-]+)/$", default_post_page_meta_description_processor ), 
+														 ))
